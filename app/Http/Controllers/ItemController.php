9c0ef72d2f27b6b38050cb6a369d52673ef6b2ac
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-13 11:16:16 +0900

    update

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -36,7 +36,8 @@ class ItemController extends Controller
     public function show(string $id)
     {
         $message = "商品IDは{$id}";
-        return $message;
+        // reources/views/item/show.blade.php を表示
+        return view('item.show');
     }
 
     /**
