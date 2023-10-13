4c2310ac0c5cd73efac14d1cabec751b26e68e66
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-13 10:23:46 +0900

    update

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+// namespace（名前空間）：packageみたいなもの
+namespace App\Http\Controllers;
+
+// プログラムの読み込み：importみたいなもの
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+}
