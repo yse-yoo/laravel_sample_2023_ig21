171a20b6fbd427a5dc5875df2d03ccb513f4c2d6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-17 13:20:56 +0900

    upate

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -4,6 +4,19 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h2>商品追加</h2>
+                    <form action="{{ route('item.store') }}" method="post">
+                        @csrf
+
+                        <div>
+                            <label for="item_name">商品名</label>
+                            <input type="text" name="name">
+                        </div>
+                        <div>
+                            <label for="item_price">価格</label>
+                            <input type="text" name="price">
+                        </div>
+                        <button>更新</button>
+                    </form>
                     <a href="{{ route('item.index') }}">戻る</a>
                 </div>
             </div>
