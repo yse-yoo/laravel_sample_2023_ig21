6bdb14eaab14c04669537ecb0ac92f5d22e83c96
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-17 11:51:37 +0900

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,13 +12,13 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('about')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
                         {{ __('About') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('search')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('search')" :active="request()->routeIs('search')">
                         {{ __('Search') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('item.index')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('item.index')" :active="request()->routeIs('item.index')">
                         {{ __('Item') }}
                     </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
