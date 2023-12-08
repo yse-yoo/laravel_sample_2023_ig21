b85d2a1988e96b3151d7d1e49bc01fbaabe7cab9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-08 11:08:27 +0900

    update

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -2,6 +2,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                @include('components.error')
+
                 <div class="p-6 text-gray-900">
                     <h2>商品編集</h2>
                     <form action="{{ route('item.update', $item->id) }}" method="post">
