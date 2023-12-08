c636c626dc75c444859d76993130298c8b77282b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-08 11:41:12 +0900

    update

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -58,7 +58,7 @@ return [
     'image' => 'The :attribute field must be an image.',
     'in' => 'The selected :attribute is invalid.',
     'in_array' => 'The :attribute field must exist in :other.',
-    'integer' => 'The :attribute field must be an integer.',
+    'integer' => ':attribute は数値で入力してください。',
     'ip' => 'The :attribute field must be a valid IP address.',
     'ipv4' => 'The :attribute field must be a valid IPv4 address.',
     'ipv6' => 'The :attribute field must be a valid IPv6 address.',
